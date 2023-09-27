50cad4122c50cb652155502cc1f93fb38a303220
JanicoMegio <janicov.m@gmail.com>
2023-09-27 15:00:02 +0800

    commit

--- my_app/my_app/settings.py
+++ my_app/my_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--n27(8kf@ls2lk56xxy(ih97qmdp%w)&9)(zq$zdqa1-+)nhvi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
